780391c9c2d05fd6b413a6f26857818ea6c15c12
shadkar <shadkar@LIN25005188.corp.capgemini.com>
2021-08-16 14:50:31 +0530

    Second Commit

---
+++ firstproject/src/main/java/firstproject/SalesClass.java
@@ -0,0 +1,13 @@
+package firstproject;
+
+public class SalesClass {
+
+		// TODO Auto-generated method stub
+		public void display() {
+			int[] arr = { 1, 2 , 3, 4};
+			for(int num:arr)
+				System.out.println(num);
+
+	}
+
+}
