b94bc065bc4ab78a07e2921071deffd996da75fb
smhadkar <sanidha.hadkar22@gmail.com>
2021-08-17 11:00:44 +0530

    workspace4 updated the loginuser

--- firstproject/src/main/java/firstproject/loginuser.java
+++ firstproject/src/main/java/firstproject/loginuser.java
@@ -1,5 +1,10 @@
 package firstproject;
 
 public class loginuser {
-
+		public static void main(String args[]) {
+		      System.out.print("Loginuser");
+		      System.out.print("Printing the String :James,Larry,Tom,Lacy");
+		}
+		
 }
+		      
